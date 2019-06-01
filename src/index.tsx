670da494290d293dada6fed325056b751a268038
karoline.ikenami <karoline.ikenami@redealumni.com>
2019-06-01 18:42:38 -0300

    add fusioncharts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+/// <reference types="./types/global" />
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/stylesheets/index.scss';
+
+import FusionCharts from 'fusioncharts/core';
+import Column2D from 'fusioncharts/viz/column2d';
+import FusionTheme from 'fusioncharts/themes/es/fusioncharts.theme.fusion';
+import ReactFC from 'react-fusioncharts';
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 
 import App from './app/App';
 
---
+++ src/types/globla.d.ts
@@ -0,0 +1,19 @@
+declare module 'fusioncharts/core' {
+  const src: any;
+  export default src;
+}
+
+declare module 'fusioncharts/viz/column2d' {
+  const src: any;
+  export default src;
+}
+
+declare module 'fusioncharts/themes/es/fusioncharts.theme.fusion' {
+  const src: any;
+  export default src;
+}
+
+declare module 'react-fusioncharts' {
+  const src: any;
+  export default src;
+}
