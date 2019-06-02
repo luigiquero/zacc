c9c7afd9cddcff100d97a3274983016ee164e494
Luigi Perotti <luigi.perotti@quero.com>
2019-06-01 21:41:05 -0300

    Adds fake request and makes login 'work'

--- src/redux/user.ts
+++ src/redux/user.ts
@@ -1,11 +1,23 @@
-import { Action } from 'redux';
+import { AnyAction } from 'redux';
+
+const USER_SET_LOGGED = 'USER_SET_LOGGED';
+
+export const userActions = {
+  setLogged: (logged = true) => ({ type: USER_SET_LOGGED, payload: logged }),
+};
 
 export const initialUserState = {
   logged: false,
 };
 
-export default function userReducer(state = initialUserState, action: Action) {
+export default function userReducer(state = initialUserState, action: AnyAction) {
   switch (action.type) {
+    case USER_SET_LOGGED:
+      return {
+        ...state,
+        logged: action.payload,
+      };
+
     default:
       return state;
   }
---
+++ src/utils/requests.tsx
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+
+const fakeResolve = (data?: any) => new Promise((res) => {
+  setTimeout(res, _.random(250, 2000, false), data);
+});
+
+const fakeReject = (error: Error) => new Promise((__, rej) => {
+  setTimeout(rej, _.random(250, 2000, false), error);
+});
+
+export const userService = {
+  async login({ email, password }: { email: string, password: string}) {
+    const validEmail = 'usuario@teste.com';
+    const validPassword = 'testeteste';
+
+    if (validEmail !== email && validPassword !== password) {
+      await fakeReject(new Error('Usuário ou senha incorretos'));
+    }
+
+    await fakeResolve();
+  }
+};
