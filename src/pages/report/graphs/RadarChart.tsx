4e3b7eaba0562c4d5d19e06812bf74da64bcd673
karoline.ikenami <karoline.ikenami@redealumni.com>
2019-06-02 00:55:22 -0300

    graficos dinamicos com base no state, yeye

--- src/pages/report/graphs/RadarChart.tsx
+++ src/pages/report/graphs/RadarChart.tsx
@@ -2,83 +2,63 @@ import FusionCharts from "fusioncharts";
 import charts from "fusioncharts/fusioncharts.charts";
 import ReactFusioncharts from "react-fusioncharts";
 import React from "react";
+import store from "../../../redux/Store";
 
 charts(FusionCharts);
 
-const dataSource = {
-  chart: {
-    caption: "Média Geral por Matérias",
-    theme: "fusion",
-    showlegend: "0",
-    showdivlinevalues: "0",
-    showlimits: "0",
-    showvalues: "1",
-    plotfillalpha: "40",
-    yaxisMaxValue: "10",
-    yaxisMinValue: "0",
-    yaxisValuesStep: "1",
-  },
-  categories: [
-    {
-      category: [
-        {
-          label: "Português"
-        },
-        {
-          label: "História"
-        },
-        {
-          label: "Geografia"
-        },
-        {
-          label: "Biologia"
-        },
-        {
-          label: "Química"
-        },
-        {
-          label: "Física"
-        },
-        {
-          label: "Matemática"
-        },
-      ]
-    }
-  ],
-  dataset: [
-    {
-      seriesname: "User Ratings",
-      data: [
-        {
-          value: "10"
-        },
-        {
-          value: "7"
-        },
-        {
-          value: "6"
-        },
-        {
-          value: "7"
-        },
-        {
-          value: "8"
-        },
-        {
-          value: "8"
-        },
-        {
-          value: "6.5"
-        },
-        {
-          value: "7.5"
-        }
-      ]
-    }
-  ]
-};
-
 export class RadarChart extends React.Component {
+  private getMateriasData() {
+    return store.getState().activities.map((activity) => {
+      return {
+        label: activity.materia
+      }
+    })
+  }
+
+  private getAverageData() {
+    return store.getState().activities.map((activity) => {
+      let average = 0;
+      let sum = 0;
+      
+      activity.provas.forEach((prova: { nota: number; }) => {
+        sum+= prova.nota;
+      })
+
+      average = sum/activity.provas.length;
+
+      return {
+        value: average
+      }
+    })
+  }
+
+
+  private dataSource = {
+    chart: {
+      caption: "Média Geral por Matérias",
+      theme: "fusion",
+      showlegend: "0",
+      showdivlinevalues: "0",
+      showlimits: "0",
+      showvalues: "1",
+      plotfillalpha: "40",
+      yaxisMaxValue: "10",
+      yaxisMinValue: "0",
+      yaxisValuesStep: "1",
+    },
+    categories: [
+      {
+        category: this.getMateriasData(),
+      }
+    ],
+    dataset: [
+      {
+        seriesname: "User Ratings",
+        data: this.getAverageData(),
+      }
+    ]
+  };
+  
   render() {
     return (
       <ReactFusioncharts
@@ -86,7 +66,7 @@ export class RadarChart extends React.Component {
         width="500"
         height="500"
         dataFormat="JSON"
-        dataSource={dataSource}
+        dataSource={this.dataSource}
       />
     );
   }
